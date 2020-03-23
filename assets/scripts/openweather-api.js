a017292c5d66e425a6beb6f126f0dc9eb4f7b8fe
Alan Ashworth <awashworth927@gmail.com>
2020-03-23 09:46:29 -0400

    clean backend.

--- assets/scripts/openweather-api.js
+++ assets/scripts/openweather-api.js
@@ -1,88 +1,3 @@
-        // Bored API --------------------------------------- API documentation @ https://www.boredapi.com/documentation
-        // Bored API - Inputs
-        var keyInput = ""; // A unique numeric id | Ex: [1000000, 3943506, 9364041, 9999999]
-        var typeInput = ""; // Type of the activity | Ex: ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
-        var participantsInput = ""; // The number of people that this activity could involve | Ex: [0, n]
-        var priceInput = ""; // A factor describing the cost of the event with zero being free | Ex: [0, 1]
-        var minpriceInput = ""; // Minimum specified price in an inclusively constrained range | Ex: [0 >= minprice <= maxprice]
-        var maxpriceInput = ""; // Maximum specified price in an inclusively constrained range | Ex: [minprice >= maxprice <= 1]
-        var accessibilityInput = ""; // A factor describing how possible an event is to do with zero being the most accessible | Ex: [0.0, 1.0]
-        var minaccessibilityInput = ""; // Minimum specified accessibility in an inclusively constrained range | Ex: [0 >= minaccessibility <= maxaccessibility]
-        var maxaccessibilityInput = ""; // Maximum specified accessibility in an inclusively constrained range | Ex: [minaccessibility >= maxaccessibility <= 1]
-
-        // Bored API - Conditions
-        var activity = "activity";
-        var randomActivity = "/";
-        var key = "?key=" + keyInput;
-        var type = "?type=" + typeInput;
-        var participants = "?participants=" + participantsInput;
-        var price = "?price=" + priceInput;
-        var minprice = "?minprice=" + minpriceInput;
-        var maxprice = "?maxprice=" + maxpriceInput;
-        var accessibility = "?accessibility=" + accessibilityInput;
-        var minaccessibility = "?minaccessibility=" + minaccessibilityInput;
-        var maxaccessibility = "?maxaccessibility=" + maxaccessibilityInput;
-
-        // Bored API - Queries: Do Not Modify
-        var queryUrl = "https://www.boredapi.com/api/";
-        var queryUrl_activity = queryUrl.concat(activity);
-
-        // Bored API - Queries: Available for Use
-        var queryUrl_randomActivity = queryUrl_activity.concat(randomActivity); // Get a random event
-        var queryUrl_key = queryUrl_activity.concat(key); // Find an activity by its key
-        var queryUrl_type = queryUrl_activity.concat(type); // Find a random activity with a given type
-        var queryUrl_participants = queryUrl_activity.concat(participants); // Find a random activity with a given number of participants
-        var queryUrl_price = queryUrl_activity.concat(price); // Find an activity with a specified price
-        var queryUrl_minPrice = queryUrl_activity.concat(minprice); // Find an event with a specified minimum price range
-        var queryUrl_maxPrice = queryUrl_activity.concat(maxprice); // Find an event with a specified maximum price range
-        var queryUrl_constrainPrice = queryUrl_activity.concat(minprice).concat("&").concat(maxprice); // Find an event with a specified constrained price
-        var queryUrl_accessibility = queryUrl_activity.concat(accessibility); // Find an activity with a specified accessibility
-        var queryUrl_minAccessibility = queryUrl_activity.concat(minaccessibility); // Find an event with a specified minimum accessibility range
-        var queryUrl_minAccessibility = queryUrl_activity.concat(maxaccessibility); // Find an event with a specified maximum accessibility range
-        var queryUrl_constrainAccessibility = queryUrl_activity.concat(minaccessibility).concat("&").concat(maxaccessibility); // Find an event with a specified constrained accessibility
-
-        // Bored API - Calls
-
-        // $.ajax({
-        //     url: queryUrl_randomActivity,
-        //     method: "GET"
-        // }).then(function(response) {
-        //     console.log(response);
-        // });
-
-        // ================================================================================================================================
-
-        // OpenWeather API - Current Weather ------------------- API documentation @ https://openweathermap.org/current
-        // OpenWeather API - Forecasted Weather -------------- API documentation @ https://openweathermap.org/forecast5
-        // OpenWeather API - Inputs
-        var appidInput = "54a9099a651206645d31affb6bbd4e54"; // API key
-        var qInput = ""; // city name | Ex: ["Atlanta", "Telluride", "New York City", "Nashville"]
-        var zipInput = ""; // zip code | Ex: [30188, 94040, 72201, 80239]
-        var unitsInput = ""; // temperature units | Ex: [default:Kelvin, imperial:Fahreneit, metric:Celsius]
-
-        // OpenWeather API - Conditions
-        var appid = "&appid=" + appidInput;
-        var weather = "weather";
-        var forecast = "forecast";
-        var q = "?q=" + qInput;
-        var zip = "?zip=" + zipInput;
-        var units = "&units=" + unitsInput;
-        var iconFilePath = "https://openweathermap.org/img/w/";
-        var iconFileType = ".png";
-
-        // OpenWeather API - Queries: Do Not Modify
-        var queryUrl = "https://api.openweathermap.org/data/2.5/";
-        var queryUrl_weather = queryUrl.concat(weather);
-        var queryUrl_forecast = queryUrl.concat(forecast);
-
-        // OpenWeather API - Queries: Available for Use
-        var queryUrl_currentWeatherAtCity = queryUrl_weather.concat(q).concat(units).concat(appid); // Get current weather by city name
-        var queryUrl_currentWeatherAtZIP = queryUrl_weather.concat(zip).concat(units).concat(appid); // Get current weather by zip code
-        var queryUrl_forecastWeatherAtCity = queryUrl_forecast.concat(q).concat(units).concat(appid); // Get forecasted weather by city
-        var queryUrl_forecastWeatherAtZIP = queryUrl_forecast.concat(zip).concat(units).concat(appid); // Get forecasted weather by zip
-
-        // Note: Items above are unimplemented code. Used only to create a 'libary' of modular ajax calls. Implemented and called below.
-
         // OpenWeather API - Calls
 
         // Call: current weather card
@@ -91,15 +6,20 @@
             let zip = "?zip=" + zipInput;
             let unitsInput = "imperial";
             let units = "&units=" + unitsInput;
+            let iconFilePath = "https://openweathermap.org/img/w/";
+            let iconFileType = ".png";
             let appidInput = "54a9099a651206645d31affb6bbd4e54";
             let appid = "&appid=" + appidInput;
+            let weather = "weather";
+            let queryUrl = "https://api.openweathermap.org/data/2.5/";
+            let queryUrl_weather = queryUrl.concat(weather);
             let queryUrl_currentWeatherAtZIP = queryUrl_weather.concat(zip).concat(units).concat(appid);
 
             $.ajax({
                 url: queryUrl_currentWeatherAtZIP,
                 method: "GET"
             }).then(function(response) {
-                console.log("Weather at zip: ", response);
+                // console.log("Weather at zip: ", response);
                 // console.log("     City name: ", response.name);
                 // console.log("  Today's date: ", moment().format("l"));
                 // console.log("          Main: ", response.weather[0].main);
@@ -115,8 +35,7 @@
                 $("#description").text(response.weather[0].description);
 
                 let weatherId = response.weather[0].id // store weather condition to adjust Bored API activity
-                console.log("Weather id: ", weatherId);
-                // let testId = 211;
+
                 ajaxCallBoredActivityFromWeather(weatherId);
 
                 function ajaxCallBoredActivityFromWeather(input){
@@ -151,22 +70,11 @@
                     let queryUrl_indoorActivity3 = queryUrl_activity.concat(indoorActivityKey3);
                     let queryUrl_indoorActivity4 = queryUrl_activity.concat(indoorActivityKey4);
 
-                    let i = 0;
+                    let i = 0; // If weather does match a bad wether, do this
                     while (i < badWeathers.length) {
                         if (input === badWeathers[i]) {
                             console.log("bad weather matched");
                             // generate indoor activity
-
-                            // if rain or other inclement weather, limit typeInput to 
-                            // education - no prevention
-                            // recreational - prevent {key: 6813070, 8503795, 8724324, 1934228, 6852505, 3149232}
-                            // social - prevent {key: 1505028, 4558850, 2085321, 2211716, 1432113}
-                            // diy - prevent {key: 8631548}
-                            // charity - prevent {key: 4894697}
-                            // cooking - no prevention
-                            // relaxation - prevent {key: 5940465, 8979931, 4290333, 9318514, 8832605, 7526324}
-                            // music - no prevention
-                            // busywork - prevent {key: 1592381}
 
                             // generate  Activity 1
                             $.ajax({
@@ -202,7 +110,7 @@
                             break;
                         }
                         i++;
-                    }
+                    } // If weather does not match a bad wether, do this
                     if (input !== badWeathers[i]) {
                         console.log("no bad weather matched");
                         // generate  Activity 1
